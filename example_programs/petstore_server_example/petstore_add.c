b55ec3d7a315aa6a4c1f56a5690f7ab526e85ff7
Hemant Zope <hemant.zope@fokus.fraunhofer.de>
2018-10-02 14:45:46 +0200

    tested sample for name in json

---
+++ example_programs/petstore_server_example/petstore_add.c
@@ -0,0 +1,108 @@
+/**
+ * test.c
+ * Small Hello World! example
+ * to compile with gcc, run the following command
+ * gcc -o test test.c -lulfius
+ */
+#include <stdio.h>
+#include <ulfius.h>
+#include <string.h>
+#include "cJSON.h"
+
+#define PORT 8080
+
+/**
+ * Callback function for the web application on /helloworld url call
+ */
+int callback_hello_world (const struct _u_request * request, struct _u_response * response, void * user_data) {
+  ulfius_set_string_body_response(response, 200, "Hello World!");
+  return U_CALLBACK_CONTINUE;
+}
+
+/**
+ * decode a u_map into a string
+ */
+char * print_map(const struct _u_map * map) {
+  char * line, * to_return = NULL;
+  const char **keys, * value;
+  int len, i;
+  if (map != NULL) {
+    keys = u_map_enum_keys(map);
+    for (i=0; keys[i] != NULL; i++) {
+      value = u_map_get(map, keys[i]);
+      len = snprintf(NULL, 0, "key is %s, value is %s", keys[i], value);
+      line = o_malloc((len+1)*sizeof(char));
+      snprintf(line, (len+1), "key is %s, value is %s", keys[i], value);
+      if (to_return != NULL) {
+        len = strlen(to_return) + strlen(line) + 1;
+        to_return = o_realloc(to_return, (len+1)*sizeof(char));
+        if (strlen(to_return) > 0) {
+          strcat(to_return, "\n");
+        }
+      } else {
+        to_return = o_malloc((strlen(line) + 1)*sizeof(char));
+        to_return[0] = 0;
+      }
+      strcat(to_return, line);
+      o_free(line);
+    }
+    return to_return;
+  } else {
+    return NULL;
+  }
+}
+
+/**
+ * Start a new counter by setting the number of sheeps to a specific value
+ * return the current number of sheeps in a json object
+ */
+int callback_sheep_counter_start (const struct _u_request * request, struct _u_response * response, void * user_data) {
+
+json_t * json_id = ulfius_get_json_body_request(request, NULL);
+json_t *obj = json_object_get(json_id,"name");
+//cJSON_Print(data);
+  char * post_params = print_map(request->map_post_body);
+
+  char * response_body = msprintf("  post parameters are \n%s\n\n  user data is %s\n\n",
+                                  json_string_value(obj), print_map(request->map_post_body));
+
+  //ulfius_set_string_body_response(response, 200, response_body);
+ulfius_set_json_body_response(response, 200, json_id);
+  o_free(post_params);
+  o_free(response_body);
+
+  return U_CALLBACK_CONTINUE;
+}
+
+/**
+ * main function
+ */
+int main(void) {
+  struct _u_instance instance;
+
+  // Initialize instance with the port number
+  if (ulfius_init_instance(&instance, PORT, NULL, NULL) != U_OK) {
+    fprintf(stderr, "Error ulfius_init_instance, abort\n");
+    return(1);
+  }
+
+  // Endpoint list declaration
+  ulfius_add_endpoint_by_val(&instance, "GET", "/helloworld", NULL, 0, &callback_hello_world, NULL);
+  ulfius_add_endpoint_by_val(&instance, "POST", "/pet", NULL, 1, &callback_sheep_counter_start, NULL);
+
+  // Start the framework
+  if (ulfius_start_framework(&instance) == U_OK) {
+    printf("Start framework on port %d\n", instance.port);
+
+    // Wait for the user to press <enter> on the console to quit the application
+    getchar();
+  } else {
+    fprintf(stderr, "Error starting framework\n");
+  }
+  printf("End framework\n");
+
+  ulfius_stop_framework(&instance);
+  ulfius_clean_instance(&instance);
+
+  return 0;
+}
